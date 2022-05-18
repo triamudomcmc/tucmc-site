516da17f1f7caa68eb35a8c06b754fbcfc8dbca8
betich <betich365@gmail.com>
2022-05-19 01:45:08 +0700

    Add og tag route info

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,41 +1,44 @@
 import PageContainer from "@components/common/PageContainer"
+import { DescribeRoute } from "@components/Meta/DescribeRoute"
 import { LocationMarkerIcon } from "@heroicons/react/solid"
 import ContactSplash from "@vectors/decorations/ContactSplash"
 import { SocialFacebook, SocialInstagram } from "@vectors/icons/Socials"
 
 const Contact = () => {
   return (
-    <div className="mx-auto min-h-screen max-w-6xl pb-10">
-      <div className="flex flex-col items-center px-6">
-        <h1 className="mt-8 text-xl font-bold md:mt-16">ติดต่อ</h1>
-        <div className="flex flex-col items-center md:flex-row">
-          <div className="mx-8 mt-6 flex flex-col items-start px-8 md:w-full md:max-w-6xl md:py-10 xl:px-0">
-            <ContactSplash className="w-[250px] md:mr-12 md:mt-0 md:w-[300px] md:max-w-sm lg:max-w-none" />
-          </div>
-          <div className="my-8 flex flex-col items-start py-0 font-medium md:h-full md:max-h-96 md:w-full md:items-start md:py-5 lg:py-0">
-            <div className="flex flex-col items-start space-y-4 px-1 ">
-              <div className="mb-1">งานกิจกรรมพัฒนาผู้เรียน (กช.)</div>
-              <div className="flex flex-row space-x-2">
-                <SocialFacebook className="h-6 w-6 text-black" />
-                <a target="_blank" href="https://www.facebook.com/triamudomclubs" className="hover:underline">
-                  TUCMC
-                </a>
-              </div>
-              <div className="flex flex-row space-x-2">
-                <SocialInstagram className="h-6 w-6 text-black" />
-                <a target="_blank" href="https://instagram.com/tucmc_official" className="hover:underline">
-                  tucmc_official
-                </a>
-              </div>
-              <div className="flex flex-row space-x-2">
-                <LocationMarkerIcon className="h-6 w-6" />
-                <h2>ตึก 50 ปี ชั้น 1 โรงเรียนเตรียมอุดมศึกษา</h2>
+    <DescribeRoute title="ติดต่อ" description="ติดต่องานกิจกรรมพัฒนาผู้เรียน (กช.)​" imgURL="/meta/banner.jpg">
+      <div className="mx-auto min-h-screen max-w-6xl pb-10">
+        <div className="flex flex-col items-center px-6">
+          <h1 className="mt-8 text-xl font-bold md:mt-16">ติดต่อ</h1>
+          <div className="flex flex-col items-center md:flex-row">
+            <div className="mx-8 mt-6 flex flex-col items-start px-8 md:w-full md:max-w-6xl md:py-10 xl:px-0">
+              <ContactSplash className="w-[250px] md:mr-12 md:mt-0 md:w-[300px] md:max-w-sm lg:max-w-none" />
+            </div>
+            <div className="my-8 flex flex-col items-start py-0 font-medium md:h-full md:max-h-96 md:w-full md:items-start md:py-5 lg:py-0">
+              <div className="flex flex-col items-start space-y-4 px-1 ">
+                <div className="mb-1">งานกิจกรรมพัฒนาผู้เรียน (กช.)</div>
+                <div className="flex flex-row space-x-2">
+                  <SocialFacebook className="h-6 w-6 text-black" />
+                  <a target="_blank" href="https://www.facebook.com/triamudomclubs" className="hover:underline">
+                    TUCMC
+                  </a>
+                </div>
+                <div className="flex flex-row space-x-2">
+                  <SocialInstagram className="h-6 w-6 text-black" />
+                  <a target="_blank" href="https://instagram.com/tucmc_official" className="hover:underline">
+                    tucmc_official
+                  </a>
+                </div>
+                <div className="flex flex-row space-x-2">
+                  <LocationMarkerIcon className="h-6 w-6" />
+                  <h2>ตึก 50 ปี ชั้น 1 โรงเรียนเตรียมอุดมศึกษา</h2>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </DescribeRoute>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,36 +6,43 @@ import projectSrc from "@images/index/projects.jpg"
 import peopleSrc from "@images/index/people.jpg"
 import TUCMCSrc from "@images/index/tucmc.jpg"
 import { TopicCard } from "@components/index/TopicCard"
+import { DescribeRoute } from "@components/Meta/DescribeRoute"
 
 const Landing: FC = () => {
   return (
-    <div className="flex min-h-screen flex-col pb-8">
-      <main className="relative">
-        <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
-          <h1 className="text-center text-6xl font-bold text-white">กช.</h1>
+    <DescribeRoute
+      title="TUCMC"
+      description="คณะกรรมการงานกิจกรรมพัฒนาผู้เรียน โรงเรียนเตรียมอุดมศึกษา"
+      imgURL="/meta/banner.jpg"
+    >
+      <div className="flex min-h-screen flex-col pb-8">
+        <main className="relative">
+          <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
+            <h1 className="text-center text-6xl font-bold text-white">กช.</h1>
 
-          <p className="mt-4 text-center text-2xl font-medium text-white">
-            คณะกรรมการงานกิจกรรมพัฒนาผู้เรียน โรงเรียนเตรียมอุดมศึกษา
-          </p>
-        </div>
+            <p className="mt-4 text-center text-2xl font-medium text-white">
+              คณะกรรมการงานกิจกรรมพัฒนาผู้เรียน โรงเรียนเตรียมอุดมศึกษา
+            </p>
+          </div>
 
-        <Image
-          className="max-h-6xl w-full object-cover"
-          src={TUCMCSrc}
-          layout="responsive"
-          // width={400}
-          height={450}
-          placeholder="blur"
-          blurDataURL={TUCMCSrc.blurDataURL}
-          alt="ประชาสัมพันธ์"
-        />
-      </main>
+          <Image
+            className="max-h-6xl w-full object-cover"
+            src={TUCMCSrc}
+            layout="responsive"
+            // width={400}
+            height={450}
+            placeholder="blur"
+            blurDataURL={TUCMCSrc.blurDataURL}
+            alt="ประชาสัมพันธ์"
+          />
+        </main>
 
-      <TopicCard mode="announcement" title="ประชาสัมพันธ์" actionText="เข้าชมงาน" imgSrc={announcementSrc} />
+        <TopicCard mode="announcement" title="ประชาสัมพันธ์" actionText="เข้าชมงาน" imgSrc={announcementSrc} />
 
-      <TopicCard title="กิจกรรม​สุดพิเศษ​" actionText="ดูผลงาน" imgSrc={projectSrc} />
-      <TopicCard title="ทำความรู้จักพวกเรา" actionText="บุคลากร" imgSrc={peopleSrc} />
-    </div>
+        <TopicCard title="กิจกรรม​สุดพิเศษ​" actionText="ดูผลงาน" imgSrc={projectSrc} />
+        <TopicCard title="ทำความรู้จักพวกเรา" actionText="บุคลากร" imgSrc={peopleSrc} />
+      </div>
+    </DescribeRoute>
   )
 }
 
