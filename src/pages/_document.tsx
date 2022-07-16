4d9e76d24d42e5a46c9c11b570730cb2da40fc02
betich <betich365@gmail.com>
2022-07-16 21:11:54 +0700

    refactor: put metadata elements in a /meta directory

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,10 +13,10 @@ export default class Document extends NextDocument {
       <Html lang="th">
         <Head>
           <meta charSet="utf-8" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/meta/site.webmanifest" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/meta/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/meta/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/meta/favicon-16x16.png" />
           <link
             rel="preload"
             href="/assets/fonts/Inter-roman.var.woff2"
